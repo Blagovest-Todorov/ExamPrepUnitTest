9ca8c182a7a71f40b2ace1520dbe67b64c24452f
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-08-11 07:38:14 +0300

    whole project

---
+++ Project/FestivalManager.Tests/StageTests.cs
@@ -0,0 +1,123 @@
+// Use this file for your unit tests.
+// When you are ready to submit, REMOVE all using statements to Festival Manager (entities/controllers/etc)
+// Test ONLY the Stage class. 
+namespace FestivalManager.Tests
+{
+    using FestivalManager.Entities;
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+
+    [TestFixture]
+	public class StageTests
+    {
+		private Stage stage = null;
+
+		[SetUp]
+		public void InitTest() 
+		{
+			stage = new Stage();		
+		}
+
+		[Test]
+	    public void PerformerCanNotBeNullInAddPerformer()
+	    {
+			Assert.Throws(typeof(ArgumentNullException), () => stage.AddPerformer(null));
+		}
+
+		[Test]
+		public void PerformerUnder18InAddPerformer() 
+		{
+			var performer = new Performer("Petar", "Petrov", 9);
+
+			Assert.Throws<ArgumentException>(() => stage.AddPerformer(performer));
+			// Assert.Throws(typeof(ArgumentException)), () => stage.AddPerformer(performer));
+		}
+
+		[Test]
+		public void PerformerAddedSuccessfullyInAddPerformer()
+		{
+			var performer = new Performer("Petar", "Petrov", 23);
+			stage.AddPerformer(performer);
+			Assert.That(stage.Performers.Count == 1);
+			Assert.That(stage.Performers.FirstOrDefault().Equals(performer));
+			
+			// Assert.Throws(typeof(ArgumentException)), () => stage.AddPerformer(performer));
+		}
+
+
+		[Test]
+		public void SongCanNotBeNullInAddSong()
+		{
+			Assert.Throws(typeof(ArgumentNullException), () => stage.AddSong(null));
+		}
+
+		[Test]
+		public void SongDurationUnder1MinuteInAddSong() 
+		{
+			var song = new Song("White rabbit", new TimeSpan(0, 0, 34));
+			Assert.Throws(typeof(ArgumentException), () => stage.AddSong(song));
+		}
+
+        [Test]
+        public void SongNameAndPerformerNameMustNotBeNullAddSongToPerformer()
+        {
+
+			Assert.Throws(typeof(ArgumentNullException), () => stage.AddSongToPerformer(null, "Pesho"));
+			Assert.Throws(typeof(ArgumentNullException), () => stage.AddSongToPerformer("WhiteR", null));
+        }
+
+		[Test]
+		public void AddSongToPerformerSongAdded() 
+		{
+			var performer = new Performer("Peter", "Petrov", 23);
+			var song = new Song("WhiteR", new TimeSpan(0, 1, 34));
+			stage.AddPerformer(performer);
+			stage.AddSong(song);
+
+			stage.AddSongToPerformer("WhiteR", "Peter Petrov");
+
+			Assert.That(performer.SongList.Count == 1);
+			Assert.That(performer.SongList.FirstOrDefault().Equals(song));
+
+		}
+
+		[Test]
+		public void StagePlayReturnRightNumbers() 
+		{
+			var performer = new Performer("Peter", "Petrov", 23);
+			var song = new Song("WhiteR", new TimeSpan(0, 1, 34));
+			stage.AddPerformer(performer);
+			stage.AddSong(song);
+
+			stage.AddSongToPerformer("WhiteR", "Peter Petrov");
+			string result = stage.Play();
+
+			Assert.That(result == "1 performers played 1 songs");
+
+		}
+
+		[Test]
+		public void AddSongToPerformerSongNotExist() 
+		{
+			var performer = new Performer("Petar", "Petrov", 23);			
+
+			stage.AddPerformer(performer);			
+
+			Assert.Throws(typeof(ArgumentException), 
+				() => stage.AddSongToPerformer("WhiteR", "Petar Petrov"));
+
+		}
+
+		[Test]
+		public void AddSongToPerformerPerformerNotExist() 
+		{
+			var song = new Song("WhiteR", new TimeSpan(0, 1, 34));
+			stage.AddSong(song);
+
+			Assert.Throws(typeof(ArgumentException), () => stage.AddSongToPerformer("WhiteR", "Petar Petrov"));
+		
+		}
+
+	}
+}
